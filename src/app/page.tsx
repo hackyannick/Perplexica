d50704bbc00fb96375bea1e41157122dfadf08fa
hackyannick <hack.yannick@gmail.com>
2025-09-04 15:12:39 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { Metadata } from 'next';
 import { Suspense } from 'react';
 
 export const metadata: Metadata = {
-  title: 'Chat - Perplexica',
-  description: 'Chat with the internet, chat with Perplexica.',
+  title: 'Chat - Search',
+  description: 'Chat with the internet, chat with Search.',
 };
 
 const Home = () => {
